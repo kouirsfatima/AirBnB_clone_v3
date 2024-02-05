d2ac48029031c50e0d3340f7e2948f32803dc7eb
MD-MADARA <mouadotaku9421@gmail.com>
2024-02-05 20:46:40 +0100

    test

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/pyhton3
 """create app_views instance"""
-from api.v1.views.index import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-# from api.v1.views.states import *
+from api.v1.views.index import *
