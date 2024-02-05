152381e12167be9d6bcb1ffb80613fa6e7a0b52b
kouirsfatima <fatimaberline535@gmail.com>
2024-02-05 22:10:15 +0100

    state.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,15 +9,13 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 @app_views.route('/states', methods=['GET'])
 @app_views.route("/states/<state_id>", methods=["GET"])
-def get_states(state_id):
+def get_states( state_id):
     states = storage.all(State).values()
     states_list = []
 
     for state in states:
         states_list.append(state.to_dict())
-
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-
     return jsonify(states_list)
