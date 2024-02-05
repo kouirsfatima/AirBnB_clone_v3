d56cc63e084628e61d3c20431d541b007020f3a0
MD-MADARA <mouadotaku9421@gmail.com>
2024-02-05 21:25:37 +0100

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,20 +2,32 @@
 """ Index file """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False, methods=["GET"])
 def status():
     """return status ok"""
     return jsonify({"status": "OK"})
 
-# from models.amenity import Amenity
-# from models.city import City
-# from models.place import Place
-# from models.review import Review
-# from models.state import State
-# from models.user import User
-# from models import storage
+@app_views.route('/stats', strict_slashes=False, methods=["GET"])
+def get_stats():
+    """retrieves the number of each objects by type"""
+    return jsonify({
+        "amenities": storage.count(Amenity), 
+        "cities": storage.count(City), 
+        "places": storage.count(Place), 
+        "reviews": storage.count(Review), 
+        "states": storage.count(State), 
+        "users": storage.count(User)
+    })
+
 
 # app = Flask(__name__)
 # app.url_map.strict_slashes = False
