a21b6e4af99ee33d9270e6adebb9be4018d31831
kouirsfatima <fatimaberline535@gmail.com>
2024-02-05 21:41:53 +0100

    fix pip8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Index file """
+
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
@@ -16,17 +17,18 @@ def status():
     """return status ok"""
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', strict_slashes=False, methods=["GET"])
 def get_stats():
     """retrieves the number of each objects by type"""
     return jsonify({
-        "amenities": storage.count(Amenity), 
-        "cities": storage.count(City), 
-        "places": storage.count(Place), 
-        "reviews": storage.count(Review), 
-        "states": storage.count(State), 
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
         "users": storage.count(User)
-    })
+        })
 
 
 # app = Flask(__name__)
