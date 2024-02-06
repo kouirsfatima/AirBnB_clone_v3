366076dcf2f423bab40765246b35bca92166e92a
MD-MADARA <mouadotaku9421@gmail.com>
2024-02-06 14:49:12 +0100

    pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 
+
 @app.teardown_appcontext
 def close_db(error):
     """closes the storage on teardown"""
