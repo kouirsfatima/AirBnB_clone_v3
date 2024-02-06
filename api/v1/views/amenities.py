527038a5bef2121b2bf98bcd23333a8817abf1ea
MD-MADARA <mouadotaku9421@gmail.com>
2024-02-06 18:19:56 +0100

    fix pycodestyle

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,7 +7,9 @@ from models import storage
 
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
-@app_views.route("/amenities/<amenity_id>", methods=["GET"], strict_slashes=False)
+@app_views.route(
+    "/amenities/<amenity_id>", methods=["GET"], strict_slashes=False
+)
 def get_amentiy(amenity_id=None):
     """Retrieves the list of all amenties"""
     if amenity_id:
@@ -24,7 +26,6 @@ def get_amentiy(amenity_id=None):
         for amenity in amenities:
             amenity_list.append(amenity.to_dict())
         return jsonify(amenity_list)
-
 
 
 @app_views.route(
@@ -56,7 +57,9 @@ def create_amenity():
     return make_response(jsonify(new_amenity.to_dict()), 201)
 
 
-@app_views.route("/amenities/<amenity_id>", methods=["PUT"], strict_slashes=False)
+@app_views.route(
+    "/amenities/<amenity_id>", methods=["PUT"], strict_slashes=False
+)
 def Update_Amenity(amenity_id):
     """update an Amenity Object"""
     amenity = storage.get(Amenity, amenity_id)
