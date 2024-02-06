7c6dd5ae799efcee2f6aa20a316561ffca4b9200
MD-MADARA <mouadotaku9421@gmail.com>
2024-02-06 14:38:48 +0100

    try2

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -36,28 +36,34 @@ def states_delete(state_id):
     return jsonify({}), 200
 
 
-@app_views.route("/states", strict_slashes=False, methods=["POST"])
+@app_views.route("/states", methods=["POST"], strict_slashes=False)
 def create_state():
-    """create a new post req"""
-    data = request.get_json(force=True, silent=True)
-    if not data:
-        abort(400, "Not a JSON")
-    if "name" not in data:
-        abort(400, "Missing name")
-    new_state = State(**data)
+    """Create a State Object"""
+    if not request.get_json():
+        return make_response("Not a JSON", 400)
+    if 'name' not in request.get_json():
+        return make_response("Missing name", 400)
+
+    data = request.get_json()
+    new_state = State(name=data["name"])
     new_state.save()
-    return jsonify(new_state.to_dict()), 201
+    return make_response(jsonify(new_state.to_dict()), 201)
 
 
-@app_views.route("/states/<state_id>", strict_slashes=False, methods=["PUT"])
-def update_state(state_id):
-    """update state"""
-    obj = storage.get(State, state_id)
-    if obj is None:
+@app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
+def Update_state(state_id):
+    """update a State Object"""
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
     data = request.get_json(force=True, silent=True)
     if not data:
         abort(400, "Not a JSON")
-    obj.name = data.get("name", obj.name)
-    obj.save()
-    return jsonify(obj.to_dict()), 200
+    keys = ["id", "created_at", "updated_at"]
+
+    for key, value in data.items():
+        if key not in keys:
+            if hasattr(state, key):
+                setattr(state, key, value)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
