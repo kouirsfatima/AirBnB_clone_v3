6f622c2ab2f6e0684422e163fe8dcade45787b8e
MD-MADARA <mouadotaku9421@gmail.com>
2024-02-07 21:41:54 +0100

    password security

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+from hashlib import md5
 
 class User(BaseModel, Base):
     """Representation of a user """
@@ -27,3 +27,13 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        """password getter"""
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        """password setter"""
+        self.__password = md5(value.encode()).hexdigest()
